b6d99ee27b63f370143273e9c63dec1e7bbc43cb
Nguyễn Phương Linh <ngplinh2005@gmail.com>
2024-09-08 15:00:28 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -4,3 +4,9 @@ let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20
 
 // Task 2: Output details
 console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${bill + tip}`)
+
+// Task 3: Create a function 
+function calculateTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20
+}
+console.log(calculateTip(100))
